22b2daa8778bd99dee6156fddb70be6bc1b1fc8b
azkiyafnii <148773221+azkiyafnii@users.noreply.github.com>
2026-06-11 00:51:10 +0700

    Refactor Counter class with new count methods
    Refactored Counter class to include methods for increasing, decreasing, and multiplying count. Updated constructor to initialize count to zero.

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,27 +1,28 @@
 package src.main.java;
 
 public class Counter {
+    private int count;
 
-	private int count;
-	
-	public Counter() {
-		this.reset();
-	}
-		
-	public void reset() {
-		count = 0;
-	}
-	
-	public void increment() {
-		count++;
-	}
-	
-	public void decrement() {
-		count--;
-	}
-	
-	public int getCount() {
-		return count;
-	}
-	
+    public Counter() {
+        this.count = 0;
+    }
+
+    public int increaseBy(int number) {
+        this.count += number;
+        return this.count;
+    }
+
+    public int decreaseBy(int number) {
+        this.count -= number;
+        return this.count;
+    }
+
+    public int multiplyBy(int number) {
+        this.count *= number;
+        return this.count;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
 }
