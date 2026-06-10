a3f398faff8dbe377847d3c7b7424d5c4e654770
azkiyafnii <148773221+azkiyafnii@users.noreply.github.com>
2026-06-11 00:58:39 +0700

    to default(1)

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,28 +1,27 @@
 package src.main.java;
 
 public class Counter {
-    private int count;
 
-    public Counter() {
-        this.count = 0;
-    }
-
-    public int increaseBy(int number) {
-        this.count += number;
-        return this.count;
-    }
-
-    public int decreaseBy(int number) {
-        this.count -= number;
-        return this.count;
-    }
-
-    public int multiplyBy(int number) {
-        this.count *= number;
-        return this.count;
-    }
-
-    public int getCount() {
-        return this.count;
-    }
+	private int count;
+	
+	public Counter() {
+		this.reset();
+	}
+		
+	public void reset() {
+		count = 0;
+	}
+	
+	public void increment() {
+		count++;
+	}
+	
+	public void decrement() {
+		count--;
+	}
+	
+	public int getCount() {
+		return count;
+	}
+	
 }
